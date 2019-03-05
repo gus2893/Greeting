14146e991dcd0858be62e6c222949a75b2350b65
Gustavo Evangelista <gus2893@gmail.com>
2019-03-05 12:37:25 -0500

    added aciton creator for Create, Updade, fetch, delete

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from '../api/streams';
-import {CHECK_AUTH, SIGN_IN, SIGN_OUT} from './types';
+import {
+    CHECK_AUTH, 
+    SIGN_IN, SIGN_OUT, 
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    DELETE_STREAM,
+    EDIT_STREAM 
+} from './types';
 
 export const trySignIn = () => async dispatch => {
     await window.gapi.load('client:auth2', ()=> {
@@ -55,5 +63,42 @@ export const trySignOut = () => async dispatch =>{
 
 
  export const createStream = formProps => async dispatch =>{
-      streams.post('/streams', formProps);
+    const response = await streams.post('/streams', formProps);
+    dispatch({
+        type: CREATE_STREAM,
+        payload: response.data
+    })
+ }
+
+ export const fetchStreams = () => async dispatch =>{
+    const response = await streams.get('/streams');
+    dispatch({
+        type: FETCH_STREAMS,
+        payload: response.data
+    })
+
+ }
+
+ export const fetchStream = id => async dispatch =>{
+     const response = await streams.get(`/streams/${id}`);
+     dispatch({
+         type:FETCH_STREAM,
+         payload: response.data
+     })
+ }
+
+ export const editStream = (id, formProps) => async dispatch => {
+     const response = await streams.put(`/streams/${id}`, formProps);
+     dispatch({
+         type: EDIT_STREAM,
+         payload: response.data
+     })
+ }
+
+ export const deleteStreams = id => async dispatch =>{
+     const reponse = await streams.delete(`/streans/${id}`);
+     dispatch({
+         type: DELETE_STREAM,
+         payload: id
+     })
  }
