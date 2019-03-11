b09a331d9661f0d6c27960b00ceb2dd20e428bf3
Gustavo Evangelista <gus2893@gmail.com>
2019-03-11 14:12:52 -0400

    Updates

---
+++ precache-manifest.25ebfd013b8de1ebdbd4d892f5bc8ebe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "493b662bacfbd7e7cc4e",
+    "url": "/Greeting/static/css/main.c3bf0029.chunk.css"
+  },
+  {
+    "revision": "493b662bacfbd7e7cc4e",
+    "url": "/Greeting/static/js/main.493b662b.chunk.js"
+  },
+  {
+    "revision": "95e9347ce4b0f16da382",
+    "url": "/Greeting/static/js/runtime~main.95e9347c.js"
+  },
+  {
+    "revision": "db30a40c3cd000df618f",
+    "url": "/Greeting/static/css/2.3be54136.chunk.css"
+  },
+  {
+    "revision": "db30a40c3cd000df618f",
+    "url": "/Greeting/static/js/2.db30a40c.chunk.js"
+  },
+  {
+    "revision": "761dcf4e4e990e7698398ad096df40cf",
+    "url": "/Greeting/index.html"
+  }
+];
